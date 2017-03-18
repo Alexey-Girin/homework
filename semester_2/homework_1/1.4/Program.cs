92622dde7bf7257d2eb79decfde5073ff6aac48e
Alexey Girin <girin.alexey@gmail.com>
2017-03-18 16:59:03 +0300

    добавил тест исправил замечания

--- semester_2/homework_1/1.4/Program.cs
+++ semester_2/homework_1/1.4/Program.cs
@@ -4,73 +4,119 @@ namespace _1._4
 {
     class Program
     {
-        static void ReadMas(ref int[,] mas, int N)
+        private static void ReadMas(int[,] mas)
         {
-            for (int i = 0; i < N; i++)
+            int size = mas.GetLength(1);
+            Console.Write("введите матрицу (разделение элементов одной строки пробелами)\n");
+            for (int i = 0; i < size; i++)
             {
-                var str = (Console.ReadLine()).Split(' ');
-                for (int t = 0; t < N; t++)
+                var strMas = Console.ReadLine().Split(' ');
+                for (int t = 0; t < size; t++)
                 {
-                    mas[i, t] = int.Parse(str[t]);
+                    mas[i, t] = int.Parse(strMas[t]);
                 }
             }
         }
 
-        static void WriteMas(int[,] mas, int N)
+        private static void TraversingMasSpirally(int[,] mas, int[] resultMas)
         {
-            int x = (N + 1) / 2 - 1;
+            int size = mas.GetLength(1);
+            int x = (size + 1) / 2 - 1;
             int y = x;
             int step = 2;
-            Console.Write(mas[x, y] + " ");
-            for(int i = 0; i < (N - 1) / 2; i++)
+
+            int currentElementOfResultMas = 0;
+            resultMas[currentElementOfResultMas] = mas[x, y];
+            currentElementOfResultMas++;
+
+            for (int i = 0; i < (size - 1) / 2; i++)
             {
                 y--;
                 for (int t = 0; t < step - 1; t++)
                 {
-                    Console.Write(mas[x, y] + " ");
+                    resultMas[currentElementOfResultMas] = mas[x, y];
+                    currentElementOfResultMas++;
                     x++;
                 }
-                Console.Write(mas[x, y] + " ");
+                resultMas[currentElementOfResultMas] = mas[x, y];
+                currentElementOfResultMas++;
 
                 y++;
                 for (int t = 0; t < step - 1; t++)
                 {
-                    Console.Write(mas[x, y] + " ");
+                    resultMas[currentElementOfResultMas] = mas[x, y];
+                    currentElementOfResultMas++;
                     y++;
                 }
-                Console.Write(mas[x, y] + " ");
+                resultMas[currentElementOfResultMas] = mas[x, y];
+                currentElementOfResultMas++;
 
                 x--;
                 for (int t = 0; t < step - 1; t++)
                 {
-                    Console.Write(mas[x, y] + " ");
+                    resultMas[currentElementOfResultMas] = mas[x, y];
+                    currentElementOfResultMas++;
                     x--;
                 }
-                Console.Write(mas[x, y] + " ");
+                resultMas[currentElementOfResultMas] = mas[x, y];
+                currentElementOfResultMas++;
 
                 y--;
                 for (int t = 0; t < step - 1; t++)
                 {
-                    Console.Write(mas[x, y] + " ");
+                    resultMas[currentElementOfResultMas] = mas[x, y];
+                    currentElementOfResultMas++;
                     y--;
                 }
-                Console.Write(mas[x, y] + " ");
+                resultMas[currentElementOfResultMas] = mas[x, y];
+                currentElementOfResultMas++;
 
                 step += 2;
-            }
-
-            Console.Write("\n");                                                                                                                                                                                
+            }                                                                                                                                                                           
         }
 
-        static void Main(string[] args)
+        private static void WriteMas(int[] mas)
         {
-            Console.Write("введите N: ");
-            int N = int.Parse(Console.ReadLine());
+            Console.Write("результат:\n");
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write(mas[i] + " ");
+            }
 
-            int[,] mas = new int[N, N];
-            ReadMas(ref mas, N);
-            WriteMas(mas, N);
-            
+            Console.Write("\n");
+        }
+
+        private static void Test()
+        {
+            const int size = 3;
+            int[,] mas = new int[size, size] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[] resultMas = new int[size * size];
+            int[] trueResult = new int[size * size] { 5, 4, 7, 8, 9, 6, 3, 2, 1 };
+
+            TraversingMasSpirally(mas, resultMas);
+            const int resultSize = size * size;
+            for (int i = 0; i < resultSize; i++)
+            {
+                if(resultMas[i] != trueResult[i])
+                {
+                    Console.Write("test false\n");
+                    return;
+                }
+            }
+            Console.Write("test true\n");
+        }
+
+        private static void Main(string[] args)
+        {
+            Test();
+            Console.Write("введите size: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] mas = new int[size, size];
+            ReadMas(mas);
+            int[] resultMas = new int[size * size];
+            TraversingMasSpirally(mas, resultMas);
+            WriteMas(resultMas);
         }
     }
 }
