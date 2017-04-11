b9b3d00a565bdce9faf20fe56416a24b923c1b8e
Alexey Girin <girin.alexey@gmail.com>
2017-04-11 17:20:39 +0300

    -написал игру стрелочками

---
+++ semester_2/homework_5/5.2(EventLoop)/5.2(EventLoop)/Game.cs
@@ -0,0 +1,56 @@
+﻿namespace EventLoopNamespace
+{
+    using System;
+
+    /// <summary>
+    /// Класс логики игры.
+    /// </summary>
+    class Game
+    {
+        /// <summary>
+        /// Движение курсора влево.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnLeft(object sender, EventArgs args)
+        {
+            if (Console.CursorLeft != 0)
+            {
+                --Console.CursorLeft;
+            }
+        }
+
+        /// <summary>
+        /// Движение курсора вправо.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnRight(object sender, EventArgs args)
+        {
+            ++Console.CursorLeft;
+        }
+
+        /// <summary>
+        /// Движение курсора вверх.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnUp(object sender, EventArgs args)
+        {
+            if (Console.CursorTop != 0)
+            {
+                --Console.CursorTop;
+            }
+        }
+
+        /// <summary>
+        /// Движение курсора вниз.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void OnDown(object sender, EventArgs args)
+        {
+            ++Console.CursorTop;
+        }
+    }
+}
