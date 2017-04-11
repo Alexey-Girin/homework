7fb316932f60d9cda9eb3c3d34f99c2ea750e7f8
Alexey Girin <girin.alexey@gmail.com>
2017-04-11 16:05:19 +0300

    -написал функции Map, Filter, Fold и юнит-тесты к ним

---
+++ semester_2/homework_5/5.1(MapFilterFold)/5.1(MapFilterFold)/Functions.cs
@@ -0,0 +1,65 @@
+﻿namespace _5thHomework.Task1
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Класс, реализующий функции.
+    /// </summary>
+    public class Functions
+    {
+        /// <summary>
+        /// Функция Map.
+        /// </summary>
+        /// <param name="list">Принимаемый список.</param>
+        /// <param name="func">Принимаемая функция.</param>
+        /// <returns>Список, полученный применением принимаемой функции к элементам принимаемого списка.</returns>
+        public List<int> Map(List<int> list, Func<int, int> func)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i] = func(list[i]);
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Функция Filter.
+        /// </summary>
+        /// <param name="list">Принимаемый список.</param>
+        /// <param name="func">Принимаемая функция.</param>
+        /// <returns>Список из элементов принимаемого списка, элементы которого удовлетворяют условию функции.</returns>
+        public List<int> Filter(List<int> list, Func<int, bool> func)
+        {
+            List<int> resultList = new List<int>();
+
+            foreach (int elem in list)
+            {
+                if (func(elem))
+                {
+                    resultList.Add(elem);
+                }
+            }
+
+            return resultList;
+        }
+
+        /// <summary>
+        /// Функция Fold/
+        /// </summary>
+        /// <param name="list">Принимаемый список.</param>
+        /// <param name="initial">Начальное значение.</param>
+        /// <param name="func">Принимаемая функция.</param>
+        /// <returns>Накопленное значение.</returns>
+        public int Fold(List<int> list, int initial, Func<int, int, int> func)
+        {
+            foreach (int elem in list)
+            {
+                initial = func(initial, elem);
+            }
+
+            return initial;
+        }
+    }
+}
