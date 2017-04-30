7268aa06a035c21e4c9246c8af62756ad6092eb7
Alexey Girin <girin.alexey@gmail.com>
2017-04-30 22:19:02 +0300

    -изменил структуру списка -написал классы исключений

--- semester_2/homework_2/2.2(List)/2.2(List).Tests/Listtests.cs
+++ semester_2/homework_2/2.2(List)/2.2(List).Tests/Listtests.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using ListNamespace;
 
     [TestClass]
     public class ListTest
@@ -14,61 +13,134 @@
         }
 
         [TestMethod]
+        public void AddToBeginningOfEmptyListTest()
+        {
+            list.Add(0, 0);
+            Assert.AreEqual(0, list.Get(0));
+        }
+
+        [TestMethod]
+        public void AddToBeginningOfListTest()
+        {
+            list.Add(0, 0);
+            list.Add(1, 0);
+            Assert.AreEqual(1, list.Get(0));
+        }
+
+        [TestMethod]
+        public void AddToEndOfListTest()
+        {
+            list.Add(0, 0);
+            list.Add(1, 1);
+            Assert.AreEqual(1, list.Get(1));
+        }
+
+        [TestMethod]
         public void AddTest()
         {
-            list.Add(1);
-            Assert.IsFalse(list.IsEmpty());
+            list.Add(0, 0);
+            list.Add(1, 1);
+            list.Add(2, 1);
+            Assert.AreEqual(2, list.Get(1));
+            Assert.AreEqual(1, list.Get(2));
         }
 
         [TestMethod]
-        public void DeleteExistingElement()
+        [ExpectedException(typeof(ActionPositionExeption))]
+        public void AddWithErrorPositionTest()
         {
-            list.Add(1);
-            list.Delete(1);
-            Assert.IsTrue(list.IsEmpty());
+            list.Add(0, 0);
+            list.Add(2, 2);
         }
 
         [TestMethod]
-        public void DeleteNonexistentElement()
-        {
-            list.Add(1);
-            list.Delete(2);
-            Assert.IsFalse(list.IsEmpty());
-        }
-
-        [TestMethod]
-        public void DeleteSome()
-        {
-            list.Add(1);
-            list.Add(2);
-            list.Delete(1);
-            list.Delete(2);
-            Assert.IsTrue(list.IsEmpty());
-        }
-
-        [TestMethod]
-        public void DeleteElementFromEmptyList()
+        [ExpectedException(typeof(EmptyListExeption))]
+        public void DeleteFromEmptyListTest()
         {
             list.Delete(0);
         }
 
         [TestMethod]
-        public void FindExistentElement()
+        [ExpectedException(typeof(EmptyListExeption))]
+        public void DeleteFromBeginningOfListTest()
         {
-            list.Add(1);
-            list.Add(2);
-            list.Add(3);
-            list.Add(4);
-            Assert.IsTrue(list.IsBelong(2));
+            list.Add(0, 0);
+            list.Delete(0);
+            list.Get(0);
         }
 
         [TestMethod]
-        public void FindNonexistentElement()
+        [ExpectedException(typeof(ActionPositionExeption))]
+        public void DeleteFromEndOfListTest()
         {
-            list.Add(1);
-            list.Add(3);
-            list.Add(4);
-            Assert.IsFalse(list.IsBelong(2));
+            list.Add(0, 0);
+            list.Add(1, 1);
+            list.Delete(1);
+            list.Get(1);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            list.Add(0, 0);
+            list.Add(1, 1);
+            list.Add(2, 2);
+
+            list.Delete(1);
+            Assert.AreEqual(2, list.Get(1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ActionPositionExeption))]
+        public void DeleteWithErrorPositionTest()
+        {
+            list.Add(0, 0);
+            list.Delete(1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EmptyListExeption))]
+        public void ChangeInEmptyListTest()
+        {
+            list.Change(0, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ActionPositionExeption))]
+        public void ChangeWithErrorPositionTest()
+        {
+            list.Add(0, 0);
+            list.Change(0, 1);
+        }
+
+        [TestMethod]
+        public void ChangeTest()
+        {
+            list.Add(0, 0);
+            list.Change(1, 0);
+            Assert.AreEqual(1, list.Get(0));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EmptyListExeption))]
+        public void GetFromEmptyListTest()
+        {
+            list.Get(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ActionPositionExeption))]
+        public void GetWithErrorPositionTest()
+        {
+            list.Add(0, 0);
+            list.Get(1);
+        }
+
+        [TestMethod]
+        public void GetTest()
+        {
+            list.Add(0, 0);
+            Assert.AreEqual(0, list.Get(0));
         }
 
         private List list;
--- semester_2/homework_2/2.2(List)/2.2(List)/List.cs
+++ semester_2/homework_2/2.2(List)/2.2(List)/List.cs
@@ -7,11 +7,26 @@
     /// </summary>
     public class List
     {
+        /// <summary>
+        /// Элемент списка.
+        /// </summary>
         private class ListElement
         {
-            public int Value;
-            public ListElement Next;
+            /// <summary>
+            /// Значение элемента списка.
+            /// </summary>
+            public int Value { get; set; }
 
+            /// <summary>
+            /// Следующий за данным элемент списка.
+            /// </summary>
+            public ListElement Next { get; set; }
+
+            /// <summary>
+            /// Конструктор экземпляра класса <see cref="ListElement"/>.
+            /// </summary>
+            /// <param name="value">Значение элемента.</param>
+            /// <param name="next">Следующий за данным элемент.</param>
             public ListElement(int value, ListElement next)
             {
                 this.Value = value;
@@ -19,77 +34,143 @@
             }
         }
 
+        /// <summary>
+        /// Головной элемент списка.
+        /// </summary>
         private ListElement head;
 
-        public List() => this.head = null;
+        /// <summary>
+        /// Размер списка;
+        /// </summary>
+        private int Size { get; set; }
 
         /// <summary>
-        /// Добавление значения в список.
+        /// Конструктор экземпляра класса <see cref="List"/>.
         /// </summary>
-        /// <param name="value">Добавляемое значение.</param>
-        public void Add(int value)
+        public List()
         {
-            ListElement newElement = new ListElement(value, head);
-            head = newElement;
         }
 
         /// <summary>
-        /// Проверка списка на пустоту.
+        /// Добавление элемента в список.
         /// </summary>
-        /// <returns>True если пусть.</returns>
-        public bool IsEmpty() => head == null;
-
-        /// <summary>
-        /// Удаление знаечния из списка.
-        /// </summary>
-        /// <param name="value">Значение удаляемого элемента.</param>
-        public void Delete(int value)
+        /// <param name="value">Значение добавляемого элемента.</param>
+        /// <param name="addPosition">Позиция добавляемого элемента.</param>
+        public void Add(int value, int addPosition)
         {
-            if (head == null)
+            ListElement newElement;
+
+            if (addPosition == 0)
             {
+                newElement = new ListElement(value, head);
+                head = newElement;
+                Size++;
                 return;
             }
 
-            if (head.Value == value)
+            if (addPosition > Size || addPosition < 0)
+            {
+                throw new ActionPositionExeption("некорректная позиция");
+            }
+
+            ListElement position = head;
+
+            for (int i = 0; i < addPosition - 1; i++)
+            {
+                position = position.Next;
+            }
+
+            newElement = new ListElement(value, position.Next);
+            position.Next = newElement;
+            Size++;
+        }
+
+        /// <summary>
+        /// Удаление элемента из списка.
+        /// </summary>
+        /// <param name="deletePosition">Позиция удаляемого элемента.</param>
+        public void Delete(int deletePosition)
+        {
+            if (Size == 0)
+            {
+                throw new EmptyListExeption("попытка обращения к элементу путого списка");
+            }
+
+            if (deletePosition >= Size || deletePosition < 0)
+            {
+                throw new ActionPositionExeption("некорректная позиция");
+            }
+
+            if (deletePosition == 0)
             {
                 head = head.Next;
+                Size--;
                 return;
             }
 
             ListElement position = head;
-            while (position.Next != null)
-            {
-                if (position.Next.Value == value)
-                {
-                    position.Next = position.Next.Next;
-                    return;
-                }
 
+            for (int i = 0; i < deletePosition - 1; i++)
+            {
                 position = position.Next;
             }
 
-            Console.WriteLine("Value not found");
+            position.Next = position.Next.Next;
+            Size--;
         }
 
         /// <summary>
-        /// Проверка на принадлежность значения списку.
+        /// Получение значения элемента в заданной позиции.
         /// </summary>
-        /// <param name="value">Проверяемое значение.</param>
-        /// <returns>True если принадлежит</returns>
-        public bool IsBelong(int value)
+        /// <param name="getPosition">Позиция элемента.</param>
+        /// <returns></returns>
+        public int Get(int getPosition)
         {
-            ListElement position = head;
-            while (position != null)
+            if (Size == 0)
             {
-                if (position.Value == value)
-                {
-                    return true;
-                }
+                throw new EmptyListExeption("попытка обращения к элементу путого списка");
+            }
 
+            if (getPosition >= Size || getPosition < 0)
+            {
+                throw new ActionPositionExeption("некорректная позиция");
+            }
+
+            ListElement position = head;
+
+            for (int i = 0; i < getPosition; i++)
+            {
                 position = position.Next;
             }
 
-            return false;
+            return position.Value;
+        }
+
+        /// <summary>
+        /// Изменение значения элемента в заданной позиции.
+        /// </summary>
+        /// <param name="value">Новое значение.</param>
+        /// <param name="changePosition">Позиция изменяемого элемента.</param>
+        public void Change(int value, int changePosition)
+        {
+            if (Size == 0)
+            {
+                throw new EmptyListExeption("попытка обращения к элементу путого списка");
+            }
+
+            if (changePosition >= Size || changePosition < 0)
+            {
+                throw new ActionPositionExeption("некорректная позиция");
+            }
+
+            ListElement position = head;
+
+            for (int i = 0; i < changePosition; i++)
+            {
+                position = position.Next;
+            }
+
+            position.Value = value;
         }
     }
 }
