0c3a33ec710002f6301755beeb1402042a24d576
Girin Alexey <girin.alexey@gmail.com>
2018-10-20 15:30:35 +0300

    наброски проекта

---
+++ semester_3/homework_3/SimpleFTP_Client/SimpleFTP_Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace SimpleFTP_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client("127.0.0.1", 8889);
+            client.List("hello, server!");
+        }
+    }
+}
