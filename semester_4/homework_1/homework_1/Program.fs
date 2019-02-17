d817fa1fd9719a245e6d126c09ca60ec986a394a
Girin Alexey <girin.alexey@gmail.com>
2019-02-17 19:25:39 +0300

    исправил замечания

--- semester_4/homework_1/homework_1/Program.fs
+++ semester_4/homework_1/homework_1/Program.fs
@@ -2,24 +2,26 @@
     match x with 
     | 0 -> 1 
     | x when x > 0 -> x * factorial (x - 1) 
-    | _ -> -1
+    | _ -> failwith "x has incorrect value"
 
 let fibonacci x = 
-    let rec iter a b i =
-        if i = 1 then b else
-        iter (a + b) (a) (i - 1)
+    let rec fibonacciNumberCalculation firstAuxiliaryValue secondAuxiliaryValue i =
+        if i = 1 then secondAuxiliaryValue else
+        fibonacciNumberCalculation (firstAuxiliaryValue + secondAuxiliaryValue) (firstAuxiliaryValue) (i - 1)
     match x with 
-    | x when x < 1 -> -1 
-    | _ -> iter 1 1 x
+    | x when x < 1 -> failwith "x has incorrect value" 
+    | _ -> fibonacciNumberCalculation 1 1 x
 
-let rev ls = ls |> List.fold (fun acc elem -> elem :: acc) []
+let reverse list = list |> List.fold (fun acc element -> element :: acc) []
 
-let createList n m =
-    let rec pow deg = 
-        match deg with
+let createListOfPowersOfTwo n m =
+    let rec getPowerOfTwo degree = 
+        match degree with
         | 0 -> 1.0
-        | deg when deg > 0 -> 2.0 * (pow (deg - 1))
-        | _ -> 1.0 / (pow -deg)
-    match m with 
-    | m when m < 1 -> []
-    | _ -> [1 .. m] |> List.map (fun x -> pow (n + x - 1))
+        | degree when degree > 0 -> 2.0 * getPowerOfTwo (degree - 1)
+        | _ -> 1.0 / (getPowerOfTwo -degree)
+    let rec createList factor acc m i =
+        if i = m then acc else
+        createList (factor * 2.0) (factor :: acc) m (i + 1)
+    if m < 0 then failwith "m has incorrect value" 
+    reverse (createList (getPowerOfTwo n) [] m 0)
