1ee0c09399148a9e1e00b173805408ae3e2d4f4d
Girin Alexey <girin.alexey@gmail.com>
2019-04-04 22:59:48 +0300

    реализовал map для деревьев

---
+++ semester_4/homework_3/homework_3/MapTree.fs
@@ -0,0 +1,12 @@
+﻿namespace Homework_3
+
+module MapTree =
+    
+    type Tree<'a> =
+    | Empty
+    | Tree of 'a * Tree<'a> * Tree<'a>
+
+    let rec map func tree =
+        match tree with
+        | Empty -> Empty
+        | Tree (node, leftSubtree, rightSubtree) -> Tree (func node, map func leftSubtree, map func rightSubtree)
