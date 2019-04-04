d9d13451ce15df8ef056c3b384f5a600935f4778
Girin Alexey <girin.alexey@gmail.com>
2019-04-04 21:43:20 +0300

    реализовал варианты функции, подсчитывающей количество четных чисел в списке

---
+++ semester_4/homework_3/homework_3/NumberOfEven.fs
@@ -0,0 +1,12 @@
+﻿namespace Homework_3
+
+module NumberOfEven = 
+
+    let evenMap list =
+        list |> Seq.map (fun element -> (abs(element) + 1) % 2) |> Seq.sum
+    
+    let evenFilter list = 
+        list |> Seq.filter (fun element -> abs(element) % 2 = 0) |> Seq.length
+
+    let evenFold list =
+        list |> Seq.fold (fun acc element -> acc + (abs(element) + 1) % 2) 0
