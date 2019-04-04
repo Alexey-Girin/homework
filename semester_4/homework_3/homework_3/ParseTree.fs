e8282ff18edea95194f251d4a477d73d6ac63935
Girin Alexey <girin.alexey@gmail.com>
2019-04-04 23:58:50 +0300

    написал дерево разбора

---
+++ semester_4/homework_3/homework_3/ParseTree.fs
@@ -0,0 +1,22 @@
+﻿namespace Homework_3
+
+module ParseTree = 
+
+    type ParseTree =
+        | Value of double 
+        | Addition of ParseTree * ParseTree
+        | Subtraction of ParseTree * ParseTree
+        | Multiplication of ParseTree * ParseTree
+        | Division of ParseTree * ParseTree
+
+    let rec eval tree = 
+        match tree with
+        | Value value -> value
+        | Addition (leftSubtree, rightSubtree) -> eval leftSubtree + eval rightSubtree
+        | Subtraction (leftSubtree, rightSubtree) -> eval leftSubtree - eval rightSubtree
+        | Multiplication (leftSubtree, rightSubtree) -> eval leftSubtree * eval rightSubtree
+        | Division (leftSubtree, rightSubtree) -> 
+            let divisor = eval rightSubtree
+            if divisor = 0.0 then raise (System.DivideByZeroException())
+            else (eval leftSubtree) / divisor
+
