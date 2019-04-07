3ea9b5bea3322f8f809d9db070f22dc2b5385bf5
Girin Alexey <girin.alexey@gmail.com>
2019-04-07 21:26:37 +0300

    написал класс для справочника

---
+++ semester_4/homework_5/homework_5/Directory.fs
@@ -0,0 +1,16 @@
+﻿namespace Homework_5
+
+module Directory =
+
+    type Directory() =
+        let mutable data : (string * string) list = List.Empty 
+        member record.Add (name, phone) = data <- [(name, phone)] @ data
+        member record.FindByName name = 
+            data |> List.fold (fun nameList record -> if fst record = name then [snd record] @ nameList else nameList) []
+        member record.FindByPhone phone = 
+            data |> List.fold (fun phoneList record -> if snd record = phone then [fst record] @ phoneList else phoneList) []
+        member record.GetAll = data
+        member record.Update newData = data <- newData
+
+
+
