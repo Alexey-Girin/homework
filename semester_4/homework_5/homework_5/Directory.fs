75c35a705f3bcf5a1ab4700837d5c3ea4e8a741b
Girin Alexey <girin.alexey@gmail.com>
2019-04-07 22:46:39 +0300

    написал основную функциональность справочника

--- semester_4/homework_5/homework_5/Directory.fs
+++ semester_4/homework_5/homework_5/Directory.fs
@@ -2,6 +2,10 @@
 
 module Directory =
 
+    open System
+    open System.IO
+    open System.Runtime.Serialization.Formatters.Binary
+
     type Directory() =
         let mutable data : (string * string) list = List.Empty 
         member record.Add (name, phone) = data <- [(name, phone)] @ data
@@ -10,7 +14,53 @@ module Directory =
         member record.FindByPhone phone = 
             data |> List.fold (fun phoneList record -> if snd record = phone then [fst record] @ phoneList else phoneList) []
         member record.GetAll = data
-        member record.Update newData = data <- newData
+        member record.Update newData = data <- newData @ data
 
+    let fileName = "Data.dat"
+    
+    let Add (directory : Directory) =
+        let record = Console.ReadLine().Split[|' '|]
+        if record.Length = 2 then directory.Add((record.[0], record.[1]))
 
+    let FindByName (directory : Directory) =
+        directory.FindByName(Console.ReadLine()) |> printfn "%A"
+    
+    let FindByPhone (directory : Directory) =
+        directory.FindByPhone(Console.ReadLine()) |> printfn "%A"
 
+    let GetAll (directory : Directory) =
+        directory.GetAll |> printfn "%A"
+
+    let SaveToFile (directory : Directory) =
+        let fsOut = new FileStream(fileName, FileMode.Create)
+        let formatter = new BinaryFormatter()
+        formatter.Serialize(fsOut, directory.GetAll)
+        fsOut.Close()
+
+    let TakeFromFile (directory : Directory) =
+        let fsIn = new FileStream(fileName, FileMode.Open)
+        let formatter = new BinaryFormatter()
+        let data = unbox<(string * string) list>(formatter.Deserialize(fsIn))
+        fsIn.Close()
+        directory.Update data
+
+    let rec requestHandler (directory : Directory) =
+        let request = Console.ReadLine()
+        match request with 
+        | "1" -> None
+        | "2" -> Add directory
+                 requestHandler directory
+        | "3" -> FindByName directory
+                 requestHandler directory
+        | "4" -> FindByPhone directory
+                 requestHandler directory
+        | "5" -> GetAll directory
+                 requestHandler directory
+        | "6" -> SaveToFile directory 
+                 requestHandler directory
+        | "7" -> TakeFromFile directory 
+                 requestHandler directory
+        | _ -> requestHandler directory
+
+    let dir = Directory()
+    requestHandler dir
