128fdd41a955c1a05c5e09d2bb4c36c33694d5a7
Girin Alexey <girin.alexey@gmail.com>
2019-04-07 22:55:46 +0300

    небольшие исправления

--- semester_4/homework_5/homework_5/Directory.fs
+++ semester_4/homework_5/homework_5/Directory.fs
@@ -6,6 +6,7 @@ module Directory =
     open System.IO
     open System.Runtime.Serialization.Formatters.Binary
 
+    ///Класс справочника
     type Directory() =
         let mutable data : (string * string) list = List.Empty 
         member record.Add (name, phone) = data <- [(name, phone)] @ data
@@ -14,36 +15,44 @@ module Directory =
         member record.FindByPhone phone = 
             data |> List.fold (fun phoneList record -> if snd record = phone then [fst record] @ phoneList else phoneList) []
         member record.GetAll = data
-        member record.Update newData = data <- newData @ data
-
+        member record.Update newData = data <- newData
+    
+    ///Имя файла
     let fileName = "Data.dat"
     
+    ///Добавить запись (имя и телефон)
     let Add (directory : Directory) =
         let record = Console.ReadLine().Split[|' '|]
         if record.Length = 2 then directory.Add((record.[0], record.[1]))
-
+    
+    ///Найти телефон по имени
     let FindByName (directory : Directory) =
         directory.FindByName(Console.ReadLine()) |> printfn "%A"
     
+    ///Найти имя по телефону
     let FindByPhone (directory : Directory) =
         directory.FindByPhone(Console.ReadLine()) |> printfn "%A"
-
+    
+    ///Вывести всё текущее содержимое базы
     let GetAll (directory : Directory) =
         directory.GetAll |> printfn "%A"
-
+    
+    ///Cохранить текущие данные в файл
     let SaveToFile (directory : Directory) =
         let fsOut = new FileStream(fileName, FileMode.Create)
         let formatter = new BinaryFormatter()
         formatter.Serialize(fsOut, directory.GetAll)
         fsOut.Close()
-
+    
+    ///Считать данные из файла
     let TakeFromFile (directory : Directory) =
         let fsIn = new FileStream(fileName, FileMode.Open)
         let formatter = new BinaryFormatter()
         let data = unbox<(string * string) list>(formatter.Deserialize(fsIn))
         fsIn.Close()
         directory.Update data
-
+    
+    ///Обработка запросов 
     let rec requestHandler (directory : Directory) =
         let request = Console.ReadLine()
         match request with 
@@ -56,11 +65,11 @@ module Directory =
                  requestHandler directory
         | "5" -> GetAll directory
                  requestHandler directory
-        | "6" -> SaveToFile directory 
+        | "6" -> SaveToFile directory
                  requestHandler directory
         | "7" -> TakeFromFile directory 
                  requestHandler directory
         | _ -> requestHandler directory
 
-    let dir = Directory()
-    requestHandler dir
+    let directory = Directory()
+    requestHandler directory |> ignore
