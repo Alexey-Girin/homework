28a887c025196cc1b8104be1fa564e6c1c6d3c45
Girin Alexey <girin.alexey@gmail.com>
2019-04-12 18:19:30 +0300

    написал getSquarе

---
+++ кр/TestTests/SquareTests.fs
@@ -0,0 +1,16 @@
+﻿namespace Test
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit.MsTest
+open Test.Square
+
+[<TestClass>]
+type SquareTests () =
+
+    [<TestMethod>]
+    member this.``Check getSquare with incorrect data`` () =
+        getSquare -1 |> should be Empty
+    
+    [<TestMethod>]
+    member this.``Check getSquare with correct data`` () =
+        getSquare 4 |> should equal ["****"; "*  *"; "*  *"; "****"]
---
+++ кр/кр/Square.fs
@@ -0,0 +1,18 @@
+﻿namespace Test
+
+module Square =
+
+    let getSquare n =
+        let getFullString n = String.init n (fun index -> "*")
+        let getString n = String.init n (fun index -> if index = 0 || index = n - 1 then "*" else " ")
+        let createLine n = List.init n (fun index -> if index = 0 || index = n - 1 then getFullString n else getString n)
+        match n with
+        | n when n <= 0 -> List.Empty
+        | _ -> createLine n
+
+    let rec print (list : List<string>) =
+        match list with
+        | [] -> true
+        | head :: tail -> printfn "%A" head; print tail
+
+    getSquare 10 |> print |> ignore
