db6062d9f47f30b37a39cad8b6086431d1f2585e
Girin Alexey <girin.alexey@gmail.com>
2019-04-26 17:51:04 +0300

    написал задачу поиска нужного палиндрома

---
+++ Test/TaskTests/FindMaxPalindromeTest.fs
@@ -0,0 +1,12 @@
+﻿namespace Test.Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit.MsTest
+open TestNamespace.FindMaxPalindrome
+
+[<TestClass>]
+type NumberOfEvenTests () =
+
+    [<TestMethod>]
+    member this.``findMaxPalindrome should return 580085`` () =
+        findMaxPalindrome () |> should equal 580085
