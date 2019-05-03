698326cc71493ca4a33ba766739e7788133b3388
Girin Alexey <girin.alexey@gmail.com>
2019-05-04 02:52:06 +0300

    написал два Workflow и тесты к ним

---
+++ semester_4/homework_7/homework_7.Tests/RoundTest.fs
@@ -0,0 +1,26 @@
+﻿namespace homework_7.Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit.MsTest
+open Homework_7.Round
+
+[<TestClass>]
+type RoundTest() =
+
+    [<TestMethod>]
+    member this.``Result of calculation should be 0.048``() = 
+        let rounding = new RoundBuilder(3)
+        rounding {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+        } |> should equal 0.048
+
+    [<TestMethod>]
+    member this.``Result of calculation should be 1.111``() = 
+        let rounding = new RoundBuilder(3)
+        rounding {
+            let! a = 5.0 / 1.5
+            let! b = 3.0
+            return a / b
+        } |> should equal 1.111
---
+++ semester_4/homework_7/homework_7/Calculate.fs
@@ -0,0 +1,14 @@
+﻿namespace Homework_7
+
+module Calculate = 
+
+    open System
+
+    /// Workflow, выполняющий вычисления с числами, заданными в виде строк
+    type CalculateBuilder() =
+        let parse string = try string |> int |> Some with | _ -> None    
+        member this.Bind(x : string, f) =
+            match parse x with
+            | None -> None
+            | Some number -> f number
+        member this.Return(x) = Some(x)
