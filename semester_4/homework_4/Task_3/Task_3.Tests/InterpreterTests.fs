208a93826cfc2da39dc647e46113b6fca8d248a7
Alexey <girin.alexey@gmail.com>
2019-06-02 23:51:09 +0300

    добавил решение задачи 4.3

---
+++ semester_4/homework_4/Task_3/Task_3.Tests/InterpreterTests.fs
@@ -0,0 +1,22 @@
+﻿namespace homework_8.Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit.MsTest
+open Homework_4.Interpreter
+
+[<TestClass>]
+type InterpreterTests () =
+
+    [<TestMethod>]
+    member this.``Check simple term`` () = 
+        let term = Application (Abstraction ('X',Variable 'X'),Abstraction ('X',Variable 'X'))
+        let result = Abstraction ('X',Variable 'X')
+        term |> reduction |> should equal result
+    
+    [<TestMethod>]
+    member this.``Check term wherein name of free and bound variables matches`` () = 
+        let term = Application
+                      (Abstraction ('x',Abstraction ('y',Application (Variable 'x',Variable 'y'))),
+                       Variable 'y')
+        let result = Abstraction ('Y',Application (Variable 'y',Variable 'Y'))
+        term |> reduction |> should equal result
